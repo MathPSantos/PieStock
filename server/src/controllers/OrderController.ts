9f8d0fe13d34a6c86fa43c61195ffa07080d7c61
MathPSantos <matheus.psantos2016@gmail.com>
2020-10-05 10:50:10 -0300

    feat: src folder added to project

---
+++ server/src/controllers/OrderController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+class OrderController {
+    async index(req: Request, res: Response) {
+
+    }
+};
+
+export default OrderController;
---
+++ server/src/controllers/PiesController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+class PiesController {
+    async index(req: Request, res: Response) {
+
+    }
+};
+
+export default PiesController;
