3a097a024d8399fd2c283419aa976eba7a9268cc
MathPSantos <matheus.psantos2016@gmail.com>
2020-12-12 10:19:06 -0300

    core: repository reseted

--- server/src/controllers/OrderController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-class OrderController {
-    async index(req: Request, res: Response) {
-
-    }
-};
-
-export default OrderController;
--- server/src/controllers/PiesController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-class PiesController {
-    async index(req: Request, res: Response) {
-
-    }
-};
-
-export default PiesController;
